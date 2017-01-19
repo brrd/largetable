4243684949024100225c0a0bfbdb2dd951d2647d
Thomas Brouard <contact@brrd.fr>
2017-01-19 12:03:25 +0100

    jQuery plugin

--- largetable.js
+++ largetable.js
@@ -1,41 +1,39 @@
-if(!jQuery) {
-    throw Error("largetable requires jQuery");
-}
+(function( $ ) {
+    $.fn.largetable = function() {
+        var scrollbarsHeight;
 
-window.largetable = function (tableSelector) {
-    tableSelector = tableSelector || "table";
-    var scrollbarsHeight;
-
-    // Add CSS rules
-    function addCSS (rule) {
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = rule;
-        document.body.appendChild(css);
-    }
-
-    // Fix shadow covering scollbars
-    function fixShadowsHeight () {
-        scrollbarsHeight = (function(div) {
-            return div.offsetHeight - div.clientHeight;
-        })($scroller.get(0));
-        if (scrollbarsHeight) {
-            addCSS(".largetable:before, .largetable:after { bottom: " + scrollbarsHeight + "px; }");
+        // Add CSS rules
+        function addCSS(rule) {
+            var css = document.createElement("style");
+            css.type = "text/css";
+            css.innerHTML = rule;
+            document.body.appendChild(css);
         }
-    }
 
-    $(function() {
-        var $tables = $(tableSelector);
+        // Fix shadow covering scollbars
+        function fixShadowsHeight() {
+            scrollbarsHeight = (function(div) {
+                return div.offsetHeight - div.clientHeight;
+            })($scroller.get(0));
+            if (scrollbarsHeight) {
+                addCSS(".largetable:before, .largetable:after { bottom: " + scrollbarsHeight + "px; }");
+            }
+        }
 
-        var renderShadows = function () {
+        // Function to render shadows
+        function renderShadows() {
             var pos = $(this).scrollLeft();
             var $container = $(this).parent(".largetable");
             $container.toggleClass("largetable-shadow-left", pos > 0);
             $container.toggleClass("largetable-shadow-right", pos < $(this).find("table").width() - $(this).width());
-        };
+        }
 
-        $tables.each(function() {
+        // Init
+        this.each(function() {
             $table = $(this);
+
+            if (!$table.is("table")) return;
+
             $table.wrap("<div class='largetable'>");
             $table.wrap("<div class='largetable-scroller'>");
             $scroller = $table.parent(".largetable-scroller");
@@ -48,5 +46,6 @@ window.largetable = function (tableSelector) {
             $scroller.scroll(renderShadows);
         });
 
-    });
-};
+        return this;
+    };
+}( jQuery ));
